6fae447548b397a04cce755afb468a495f656b3f
spaderdabomb <jpdodson42@gmail.com>
2024-08-16 16:08:01 -0700

    Test commits from laptop

--- Assets/Scripts/Environment/WorldTree.cs
+++ Assets/Scripts/Environment/WorldTree.cs
@@ -6,6 +6,7 @@ using UnityEngine.UIElements;
 
 public class WorldTree : MonoBehaviour
 {
+    private HealthBar _healthBar = null;
     private void OnEnable()
     {
         InputManager.Instance.playerActionsInput.OnPunchHit += DamageTree;
@@ -21,8 +22,14 @@ public class WorldTree : MonoBehaviour
         if (!GameObjectManager.Instance.playerInteract.interactingObjects.Contains(gameObject))
             return;
 
+        if (_healthBar == null)
+            MakeHealthBar();
+    }
+
+    private void MakeHealthBar()
+    {
         VisualElement healthBarInst = UiManager.Instance.healthBar.Instantiate();
-        HealthBar healthBar = new HealthBar(healthBarInst, gameObject, 100f);
+        _healthBar = new HealthBar(healthBarInst, gameObject, 100f);
         UiManager.Instance.uiGameManager.root.Add(healthBarInst);
     }
 }
